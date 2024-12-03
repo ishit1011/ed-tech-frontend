137b26711c1ec463639d5dabc716a0dc0740fb8d
Ishit Singh <100533099+ishit1011@users.noreply.github.com>
2024-12-03 21:36:58 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import './App.css'
 import { UserContextProvider } from './context/UserContext.jsx'
 import { CourseContextProvider } from './context/CourseContext.jsx'
 
-export const server = 'http://localhost:5000' // backend server PORT
+export const server = 'https://ed-tech-server-gfhp.onrender.com' // backend server PORT
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
